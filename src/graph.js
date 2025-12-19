c0dacde29d8a4a91236947205659c4978a240b27
pranjal.arora <pranjal.arora@officebanao.com>
2025-12-19 12:35:33 +0530

    first commit

---
+++ src/graph.js
@@ -0,0 +1,14 @@
+import { StateAnnotation } from "./state";
+import { StateGraph } from "@langchain/langgraph";
+
+const frontDeskAgent = (state) => {
+    return 'Frontdesk agent'
+}
+
+const frontDeskAgent = (state) => {
+    return 'Frontdesk agent'
+}
+
+
+
+const graph = new StateGraph(StateAnnotation);
---
+++ src/model.js
@@ -0,0 +1,6 @@
+import { ChatGroq } from "@langchain/groq";
+
+export const model = new ChatGroq({
+    model: 'openai/gpt-oss-120b',
+    temperature: 0
+})
